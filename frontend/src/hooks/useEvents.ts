aa3724af535e029c403800b09bd56b67ab0081af
Dmitry Popov <dmitry.popov.2002@gmail.com>
2023-10-30 21:16:57 -0300

    changes

--- frontend/src/hooks/useEvents.ts
+++ frontend/src/hooks/useEvents.ts
@@ -3,13 +3,28 @@ import axios from 'axios';
 import { useAppSelector, useAppDispatch } from "../store";
 import { Event, setEvents, addEvent, deleteEventById } from "../store/slicers/eventsSlice"
 
+function getInitialLoadedMonths() {
+    const currentDate = new Date(Date.now());
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+    return [
+        `${year - (month - 1 < 0 ? 1 : 0)}-${(month - 1) % 12}`,
+        `${year}-${month}`,
+        `${year + (month + 1 > 12 ? 1 : 0)}-${(month + 1) % 12}`
+    ];
+}
+
 export function useEvents() {
     const events = useAppSelector((state) => state.events.events);
     const dispatch = useAppDispatch();
+    let loadedMonths: string[] = [];
+    if(events) {
+        loadedMonths = Object.keys(events);
+    }
 
     useEffect(() => {
         if (!events) {
-            axios.post("/api/events")
+            axios.post<Event[]>("/api/events", getInitialLoadedMonths())
                 .then((res) => {
                     dispatch(setEvents(res.data));
                 })
@@ -23,9 +38,16 @@ export function useEvents() {
         dispatch(addEvent(event));
     }
 
-    function deleteEvent(id: string){
+    function deleteEvent(id: string) {
         dispatch(deleteEventById(id));
     }
 
-    return { events: [{id: "0", title: "Event", start: "2023-10-28"}], newEvent, deleteEvent };
+    function newMonth(month: string) {
+        axios.post<Event[]>("/api/events", [month])
+                .then((res) => {
+                    dispatch(addEvent(res.data[0]));
+                });
+    }
+
+    return { events: [{ id: "0", title: "Event", start: "2023-10-28" }], newEvent, deleteEvent, newMonth };
 }
--- frontend/src/store/slicers/eventsSlice.ts
+++ frontend/src/store/slicers/eventsSlice.ts
@@ -6,29 +6,52 @@ export type Event = {
     start: string
 }
 
+export type MonthEvents = {
+    [month: string]: Event[]
+}
+
 export type InitialState = {
-    events: Event[] | null | undefined
+    events: MonthEvents | null | undefined
 }
 const initialState: InitialState = {
     events: undefined
 };
+
+function cutDay(date: string) {
+    const splitedDate = date.split('-');
+    return `${splitedDate[0]}-${splitedDate[1]}`;
+}
+
+function safePush(obj: MonthEvents, key: string, item: Event) {
+    if (!obj[key]) obj[key] = [];
+    obj[key].push(item);
+}
+
+function eventsListToMonthEvents(eventsList: Event[] | null) {
+    if (!eventsList) return null;
+    const monthEvents: MonthEvents = {};
+    eventsList.forEach((val) => { safePush(monthEvents, cutDay(val.start), val) });
+    return monthEvents;
+}
 
 export const eventSlice = createSlice({
     name: 'events',
     initialState,
     reducers: {
         setEvents: (state, action: PayloadAction<Event[] | null>) => {
-            state.events = action.payload;
+            state.events = eventsListToMonthEvents(action.payload);
         },
         addEvent: (state, action: PayloadAction<Event>) => {
-            if(!state.events) throw new Error("'events' is not initialaized");
-            state.events.push(action.payload);
+            if (!state.events) throw new Error("'events' is not initialaized");
+
+            safePush(state.events, cutDay(action.payload.start), action.payload);
         },
         deleteEventById: (state, action: PayloadAction<string>) => {
-            if(!state.events) throw new Error("'events' is not initialaized");
-            const isSameId = (el: Event) => el.id === action.payload;
-            const elIndex = state.events.findIndex(isSameId);
-            state.events.splice(elIndex, 0);
+            if (!state.events) throw new Error("'events' is not initialaized");
+
+            const eventsList = Object.values(state.events).flat();
+            const elIndex = eventsList.findIndex((el: Event) => el.id === action.payload);
+            state.events = eventsListToMonthEvents(eventsList.splice(elIndex, 0));
         }
     }
 });
